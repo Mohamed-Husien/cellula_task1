b263686716a80b91840a7ddf3597f49d035501c4
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-02-12 10:09:54 +0200

    on boarding feature done

---
+++ lib/core/widgets/cutom_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    this.borderRadius,
+    required this.text,
+    this.fontSize,
+    this.onPressed,
+  });
+  final Color backgroundColor;
+  final Color textColor;
+  final BorderRadius? borderRadius;
+  final String text;
+  final double? fontSize;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 64,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ?? BorderRadius.circular(32),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(color: textColor, fontSize: fontSize),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/onboarding_feature/presentation/views/onboarding_view.dart
+++ lib/features/onboarding_feature/presentation/views/onboarding_view.dart
@@ -1,5 +1,6 @@
 import 'package:cellula_task1_app/constants.dart';
 import 'package:cellula_task1_app/core/utils/app_images.dart';
+import 'package:cellula_task1_app/core/widgets/cutom_button.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingView extends StatelessWidget {
@@ -7,31 +8,49 @@ class OnBoardingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(Assets.imagesTest),
-                fit: BoxFit.cover,
-              ),
+    return Flexible(
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.imagesTest),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black12,
+                Color(0xff000F30),
+              ],
             ),
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.black12,
-                    kPrimaryColor,
-                  ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 24, bottom: 24),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Expanded(
+                  flex: 3,
+                  child: Center(),
                 ),
-              ),
+                Expanded(
+                  flex: 2,
+                  child: CustomButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.white,
+                    textColor: kPrimaryColor,
+                    text: 'Skip',
+                    fontSize: 18,
+                  ),
+                )
+              ],
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
